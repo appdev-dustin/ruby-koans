c1a903d8f64c91a32a413cef54bb3fe1b929cd0e
sgnlcntxt <dieckmann.dustin@gmail.com>
2022-04-05 18:58:26 +0000

    Commit 15

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -16,9 +16,17 @@ class Proxy
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
   end
 
   # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    @messages.push(method_name)
+    @object.send(method_name, *args, &block)
+  end
+  def messages
+    @messages
+  end
 end
 
 # The proxy object should pass the following Koan:
